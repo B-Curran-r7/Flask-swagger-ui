90b5bab548d2c4a63ed4329d81ad38ec27b623b9
B-Curran-r7 <barry_curran@rapid7.com>
2025-01-14 18:34:12 +0000

    Basic auth
    Test with auth, 401 does not happen yet

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if request.headers.get('Authorization') == 'Bearer secret-from-store':
+        response = make_response(render_template('index.html'))
+        return response, 200
+    return make_response(render_template('index.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
