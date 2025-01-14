186f343125e872efc48a3b6b947affe1bda2fa75
B-Curran-r7 <barry_curran@rapid7.com>
2025-01-14 18:56:12 +0000

    Not debug
    Trying to get this to run on Azure

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print("Request received.")
     if request.headers.get('Authorization') == 'Bearer secret-from-store':
         response = make_response(render_template('index.html'))
         return response, 200
     return make_response(render_template('index.html'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
