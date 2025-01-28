10c895c8ed8a3d281ebc5a59f60ba2c54f38e12a
B-Curran-r7 <barry_curran@rapid7.com>
2025-01-28 18:39:21 +0000

    Managed Identity test
    In theory, this should be able to get a secret by a name since it is in the right MI group and I have specified the id

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, request, render_template, make_response
+from azure.keyvault.secrets import SecretClient
+from azure.identity import ManagedIdentityCredential
+
+credential = ManagedIdentityCredential(client_id="fb9b4fc1-7856-4b09-ac12-713345ecd510")
+
+#Todo, replace with environment variable after testing
+secretClient = SecretClient(vault_url="https://key-vault-in-mi-group.vault.azure.net/", credential=credential)
+secret = secretClient.get_secret("test-secret")
+print(secret.value)
+print(secret.name)
+print(secret.id)
 
 app = Flask(__name__)
 
